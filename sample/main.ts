39f6b2ee7bda2630d6c53ff87dd2815c4756c43c
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2023-10-22 11:10:20 +0900

    created project

---
+++ sample/main.ts
@@ -0,0 +1,27 @@
+import { Construct } from "constructs";
+import { App, TerraformStack, TerraformOutput } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Instance } from "@cdktf/provider-aws/lib/instance";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new AwsProvider(this, "AWS", {
+      region: "ap-northeast-1",
+    });
+
+    const ec2Instance = new Instance(this, "compute", {
+      ami: "ami-0d48337b7d3c86f62", // Amazon Linux 2023
+      instanceType: "t2.micro",
+    });
+
+    new TerraformOutput(this, "public_ip", {
+      value: ec2Instance.publicIp,
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, "aws_instance");
+app.synth();
